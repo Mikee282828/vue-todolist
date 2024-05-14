ef6309311127beee755503489b3acea98c7fc6a2
MikeDesktop <micheledong2803@gmail.com>
2024-05-14 18:33:36 +0200

    bonus1

--- main.js
+++ main.js
@@ -3,24 +3,37 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            lista : [
-                "mangiare la pizza",
-                "fare la lavatrice",
-                "fare gli auguri a Giuditta"
-            ],
-            nuovo : null,
-            clicked : true
+            // lista : [
+            //     "mangiare la pizza",
+            //     "fare la lavatrice",
+            //     "fare gli auguri a Giuditta"
+            // ],
+            listaBonus: [
+                {
+                    titolo: "mangiare la pizza",
+                    done: "false"
+                },
+                {
+                    titolo: "fare la lavatrice",
+                    done: "false"
+                },
+                {
+                    titolo: "fare gli auguri a Giuditta",
+                    done: "false"
+                }],
+            nuovo: "",
+            clicked: true
         }
     },
-    methods:{
-        addTask(task){
-            console.log(`L'elemento aggiunto è: ${task}`);
-            this.lista.push(task);
-            setTimeout(function(){
+    methods: {
+        addTask(task) {
+            let oggetto = {titolo: task, done: "false"};
+            this.listaBonus.push(oggetto);   
+            setTimeout(function () {
                 document.querySelector("input").value = "";
-            },10);
+            }, 10);
         },
-        done(index){
+        done(index) {
             let listItem = document.querySelector(`li:nth-of-type(${index})`);
             listItem.classList.add("d-none");
         }
